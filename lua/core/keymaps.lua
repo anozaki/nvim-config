cb5aad8b134ffd374d4cc33cf03101ac44e45c64
Akito Nozaki <akito@tanoshi.net>
2023-11-08 08:46:06 -0800

    add terminal and adjust config

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,12 +1,12 @@
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<cr>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<cr>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<cr>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<cr>")
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.keymap.set('n', '<esc>', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>h", ":nohlsearch<cr>")
+vim.keymap.set("n", "<esc>", ":nohlsearch<cr>")
 
-vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set("n", "J", "mzJ`z")
 
-vim.keymap.set('n', '<c-d>', '<c-d>zz')
-vim.keymap.set('n', '<c-u>', '<c-u>zz')
+vim.keymap.set("n", "<c-d>", "<c-d>zz")
+vim.keymap.set("n", "<c-u>", "<c-u>zz")
